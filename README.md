341402989801e142212a80541e0d2e21f0744bab
Craig <craig@oppkey.com>
2024-12-13 10:10:27 -1000

    working on data structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Carlsbad Cavern Tour with Marzipano
+
+Photostream
+
+https://www.flickr.com/photos/193079646@N03/with/52283079899
---
+++ data/data.js
@@ -0,0 +1,44 @@
+const data = [
+    {
+        filename: "carlsbad_0.jpg",
+        yaw: 0,
+        pitch: 0,
+        hotspots: [ 
+            {
+            yaw: 0,
+            pitch: 0,
+            switchTo: 1
+        },
+    ]
+    },
+    {
+        filename: "carlsbad_1.jpg",
+        yaw: 0,
+        pitch: 0,
+        hotspots: [{
+            yaw: 0,
+            pitch: 0,
+            switchTo: 2
+        }]
+    },
+    {
+        filename: "carlsbad_2.jpg",
+        yaw: 0,
+        pitch: 0,
+        hotspots: [{
+            yaw: 0,
+            pitch: 0,
+            switchTo: 3
+        }]
+    },
+    {
+        filename: "carlsbad_3.jpg",
+        yaw: 0,
+        pitch: 0,
+        hotspots: [{
+            yaw: 0,
+            pitch: 0,
+            switchTo: 1
+        }]
+    },
+];
